4944c76982a89fc605fc35c7903ced4b22369081
TrezorTop <trezortop92@gmail.com>
2020-05-21 16:48:42 +0300

    buddies gram -> easy study

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BuddiesGram',
+      title: 'EasyStudy',
       debugShowCheckedModeBanner: false,
       theme: ThemeData
       (
