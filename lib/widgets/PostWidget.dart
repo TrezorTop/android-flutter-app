32e7535d46f56f2de2b7cae1d0e9f445f48115d7
TrezorTop <trezortop92@gmail.com>
2020-05-21 16:30:02 +0300

    Revert "Посты в профиле"
    This reverts commit fae4016e1f5b16ff06b1450d99ad6d3f2f605518.

--- lib/widgets/PostWidget.dart
+++ lib/widgets/PostWidget.dart
@@ -1,5 +1,6 @@
 import 'package:buddiesgram/models/user.dart';
 import 'package:buddiesgram/pages/HomePage.dart';
+import 'package:buddiesgram/widgets/CImageWidget.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +11,7 @@ class Post extends StatefulWidget
 {
   final String postId;
   final String ownerId;
-  final dynamic likes;
+  Map likes;
   final String username;
   final String description;
   final String location;
